bcd8cd84d7de712311ba74cf188b881df1130a91
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-25 20:37:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,19 +19,14 @@ class Transfer
   
   
 def execute_transaction
-  
-  
   if @sender.balance > @amount && @status == "pending" && valid?
       @sender.deposit(@amount * -1)
       @receiver.deposit(@amount)
       @status = "complete"
-    
   else
     @status = "rejected"
     "Transaction rejected. Please check your account balance."
-      
     end
-  
   end
   
   
